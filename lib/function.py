514eec8076ff3a18d42b8e040764c136da9d2f29
mararo-ops <danielmararo1@gmail.com>
2023-11-20 23:34:12 +0300

    completed assignment

---
+++ lib/function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def greet_programmer():
+    print("Hello, programmer!")
+
+def greet(name):
+    print(f"hello ${name}")
+
+def greet_with_default(name="programmer"):
+    print(f"hello ${name}")
+
+def add(num1, num2):
+    return num1+num2
+
+def halve(number):
+    return number/2
